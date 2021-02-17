13a59a68239f293b73f4e38b1798398385c98d3d
Jamal Hussain <jamalhussayn@gmail.com>
2021-02-18 00:24:55 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/popcornreviews'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/popcornreviews/'}),
-);
+app.use(express.static(__dirname + '/dist/popcornreviews'));
+app.get('/*',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/dist/popcornreviews/index.html'));
+});
+// app.get('/*', (req, res) =>
+// res.sendFile(path.join(__dirname+'/dist/myapp/index.html'));});
+// );
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
