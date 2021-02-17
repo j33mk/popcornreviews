8d332cb5dce45d0fb8f72e10dd36b56ac680cc37
Jamal Hussain <jamalhussayn@gmail.com>
2021-02-18 00:12:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/popcornreviews'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/popcornreviews/'}),
 );
 
 // Start the app by listening on the default Heroku port
